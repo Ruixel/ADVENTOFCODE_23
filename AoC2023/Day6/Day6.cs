9c899c950371e011e22b247fd8b0dfa6736e0d71
Ruixel <13946497+Ruixel@users.noreply.github.com>
2023-12-09 19:00:15 +0000

    Day 6 & 9

---
+++ AoC2023/Day6/Day6.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace AoC2023.Day6;
+
+public class Day6
+{
+	public static void Solve()
+	{
+		var input = System.IO.File.ReadAllText("../../../Day6/Day6Test.txt")
+			.Split("\r\n");
+		
+		// var time = input.First().Split(':')[1].Split(" ").Select(s => s.Trim()).Select(int.Parse).ToList();
+		// var dist = input.Skip(1).First().Split(':')[1].Split(" ").Select(s => s.Trim()).Select(int.Parse).ToList();
+        
+		// var time = new List<int>(){7, 15, 30};
+		// var dist = new List<int>() {9, 40, 200};
+
+        // Part 1
+		//var time = new List<int>() {61, 67, 75, 71};
+		//var dist = new List<int>() {430, 1036, 1307, 1150};
+		
+		// var time = new List<int>(){71530};
+		// var dist = new List<int>(){940200};
+		
+		var time = new List<long>() {61677571};
+		var dist = new List<long>() {430103613071150};
+
+		var part1 = 1;
+		for (var race = 0; race < time.Count; race++)
+		{
+			var wins = 0;
+			for (var x = 0.0d; x < time[race]; x++)
+			{
+				var value = -Math.Pow(x, 2) + time[race]*x - dist[race];
+				//Console.WriteLine($"Race {race}: {x} -> {value}");
+				if (value > 0)
+				{
+					wins++;
+				}	
+			}
+			
+			part1 *= wins;
+		}
+		
+
+		Console.WriteLine($"Part1: {part1}");
+	}
+}
---
+++ AoC2023/Day9/Day9.cs
@@ -0,0 +1,61 @@
+﻿namespace AoC2023.Day9;
+
+public class Day9
+{
+	public static void Solve()
+	{
+		var input = System.IO.File.ReadAllText("../../../Day9/Day9.txt")
+			.Split("\r\n")
+			.Select(l => l.Split().Select(int.Parse).ToList())
+			.ToList();
+
+		var part1 = 0;
+		var part2 = 0;
+		foreach (var seq in input)
+		{
+			var seqs = new List<List<int>>();
+			seqs.Add(seq);
+
+			var seqIndex = 0;
+			var isLinear = false;
+			while (!isLinear)
+			{
+				var diffs = seqs[seqIndex].Skip(1).Select((i, idx) => i - seqs[seqIndex][idx]).ToList();
+				seqs.Add(diffs);
+				
+				isLinear = diffs.All(d => d == 0);
+				seqIndex++;
+			}
+			
+			Console.WriteLine($"Found linear sequence of length {seqs.Count} at index {seqIndex}");
+			
+			// Find the next number
+			seqIndex--;
+			while (seqIndex >= 0)
+			{
+				var next = seqs[seqIndex].Last() + seqs[seqIndex + 1].Last();
+				seqs[seqIndex].Add(next);
+				
+				seqIndex--;
+			}
+			
+			// Find the previous number
+			seqIndex = seqs.Count - 2;
+			while (seqIndex >= 0)
+			{
+				var prev = seqs[seqIndex].First() - seqs[seqIndex + 1].First();
+				seqs[seqIndex].Insert(0, prev);
+				
+				seqIndex--;
+			}
+
+
+			part1 += seqs[0].Last();
+			part2 += seqs[0].First();
+		}
+
+		Console.WriteLine($"Part1: {part1}");
+		Console.WriteLine($"Part2: {part2}");
+		
+	}
+}
